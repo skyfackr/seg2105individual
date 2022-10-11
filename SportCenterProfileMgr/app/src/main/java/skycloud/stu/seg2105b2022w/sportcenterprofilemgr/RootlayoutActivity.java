20430161594150c1c09a7427cb3876c4ba71b343
fackr jiang <44115200+skyfackr@users.noreply.github.com>
2022-10-10 20:18:59 -0400

    set button function

--- SportCenterProfileMgr/app/src/main/java/skycloud/stu/seg2105b2022w/sportcenterprofilemgr/RootlayoutActivity.java
+++ SportCenterProfileMgr/app/src/main/java/skycloud/stu/seg2105b2022w/sportcenterprofilemgr/RootlayoutActivity.java
@@ -2,7 +2,11 @@ package skycloud.stu.seg2105b2022w.sportcenterprofilemgr;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class RootlayoutActivity extends AppCompatActivity {
 
@@ -10,5 +14,13 @@ public class RootlayoutActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.rootlayout);
+        findViewById(R.id.mapBtn).setOnClickListener(v -> {
+            EditText address = findViewById(R.id.teamZIP);
+            Uri gmmIntentUri = Uri.parse("geo:0,0?q=" + address.getText().toString());
+            Intent mapIntent = new Intent(Intent.ACTION_VIEW, gmmIntentUri);
+            mapIntent.setPackage("com.google.android.apps.maps");
+            startActivity(mapIntent);
+        });
     }
+
 }
